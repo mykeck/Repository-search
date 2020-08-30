813f0d99b59393f9fcc86b298cb014a333f8a9c0
MikeC <kiragumike@gmail.com>
2020-08-30 09:23:05 +0300

    added a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServicesComponent } from './services/profile.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
